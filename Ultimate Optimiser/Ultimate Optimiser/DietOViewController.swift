edaa4b8e5aa24995c7c1662c9501ebca0ddf9bad
Anton Dominique Cruz <adtcrz@gmail.com>
2016-11-28 23:30:06 +0800

    Added View Controller for the Diet Optimiser feature

---
+++ Ultimate Optimiser/Ultimate Optimiser/DietOViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DietOViewController.swift
+//  Ultimate Optimiser
+//
+//  Created by Anton Dominique Cruz on 28/11/2016.
+//  Copyright © 2016 adtcruz. All rights reserved.
+//
+
+import Cocoa
+
+class DietOViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
